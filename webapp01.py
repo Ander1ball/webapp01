1ac74c537252515a1eae920c971bb1f04b2ddf8b
Ander1ball <116382092+Ander1ball@users.noreply.github.com>
2022-10-21 23:22:25 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -8,15 +8,18 @@ image01 = Image.open('download.jfif')
 st.title("!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("site do balacubaco")
+st.header("Minecraft")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("por que o jog é bom pra pensar")
+st.subheader("por que o jog é bom pra pensar?")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("O jogo minecraft é famosso em multiplas plataformas de gaming ou plataformas de streming")
 
-st.subheader("------ **Desenvolvido por: Massaki de O. Igarashi** -----")
+st.subheader("O jogo conhecido como o jogo quadrado teve o seu lançamento em 18 de novembro de 2011, onde foi notado muitas caracteristicas marcantes do jogo,como por exemplo, o fato dele ser totalmente quadrado em até mesmo em seus personagens.")
+st.subheader("Seu personagens no inicio eram somente um o canonico Steve, mas em setembro de 2014 foi adicionada a personagem Alex.") 
+image01 = Image.open('steve-e-alex-minecraft.webp')
+
 
 menu = ["Texto_Colunas",
         "Texto_Markdown",
