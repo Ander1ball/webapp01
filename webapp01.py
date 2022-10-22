cb65fe9cf0fc00461583c152ac01796da7c0d788
Ander1ball <116382092+Ander1ball@users.noreply.github.com>
2022-10-21 23:34:15 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from PIL import Image
 
-image01 = image.open('11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.webp')
+image01 = image01.open('11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.webp')
                      
 # Use st.title("Minecraft") para adicionar um TÍTULO ao seu Web app
 st.title("Minecraft")
