db7a935c3a7d4865f25d516e8df5e06a1e1f47fa
Ander1ball <116382092+Ander1ball@users.noreply.github.com>
2022-10-21 23:40:33 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -2,7 +2,9 @@
 #import the libraries
 import streamlit as st
 from PIL import Image
-                     
+
+image02 = Image.open('steve-e-alex-minecraft.webp')
+
 # Use st.title("Minecraft") para adicionar um TÍTULO ao seu Web app
 st.title("Minecraft")
 
@@ -13,13 +15,11 @@ st.subheader("por que o jogo é bom pra pensar?")
 st.write("O jogo minecraft é famosso em multiplas plataformas de gaming ou plataformas de streming")
 
 st.subheader("O jogo conhecido como o jogo quadrado teve o seu lançamento em 18 de novembro de 2011, onde foi notado muitas caracteristicas marcantes do jogo,como por exemplo, o fato dele ser totalmente quadrado em até mesmo em seus personagens.")
-st.subheader("Seu personagens no inicio eram somente um o canonico Steve, mas em setembro de 2014 foi adicionada a personagem Alex.") 
-image02 = Image.open('steve-e-alex-minecraft.webp')
+st.subheader("Seu personagens no inicio eram somente um o canonico Steve, mas em setembro de 2014 foi adicionada a personagem Alex.")
 
-
-menu = esc ["Texto_Colunas",
-            "Texto_Markdown",
-            "Inserir_Figura"]
+menu = ["Texto_Colunas",
+        "Texto_Markdown",
+        "Inserir_Figura"]
 choice = st.sidebar.selectbox("Menu de Opções",menu)
 st.sidebar.write("Texto Side Bar")
     
